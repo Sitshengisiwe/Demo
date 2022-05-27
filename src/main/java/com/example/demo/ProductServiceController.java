a0df3d5d3d3d60183c041cdc09b21aa85913ce84
Sitshengisiwe <patience.s.nkomo@gmail.com>
2022-05-27 11:27:57 +0200

    Patience Demo File

---
+++ src/main/java/com/example/demo/ProductServiceController.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+    @RestController
+
+    public class ProductServiceController {
+        @Autowired
+        ProductRepository productRepository;
+        @RequestMapping ("/products")
+        public ResponseEntity<List<Product>> getAllProducts(@RequestParam(required = false) String name) {
+            //try {
+            List<Product> Products = new ArrayList<Product>();
+            if (name == null)
+                productRepository.findAll().forEach(Products::add);
+            else
+                productRepository.findByName(name).forEach(Products::add);
+            if (Products.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(Products, HttpStatus.OK);
+
+        }
+    }
